0e2d85ee2e975b8cebd5fe301bb2c627a81a17b0
falkTX <falktx@gmail.com>
2016-11-18 00:55:58 +0000

    Fix possible ambiguous use of pow()

--- exported-code/gen_dsp/genlib_ops.h
+++ exported-code/gen_dsp/genlib_ops.h
@@ -147,7 +147,7 @@ inline t_sample safepow(t_sample base, t_sample exponent) {
 inline t_sample absdiff(t_sample a, t_sample b) { return fabs(a-b); }
 
 #ifndef WIN32
-inline t_sample exp2(t_sample v) { return pow(2., v); }
+inline t_sample exp2(t_sample v) { return pow(t_sample(2.), v); }
 
 inline t_sample trunc(t_sample v) {
 	t_sample epsilon = (v<0.0) * -2 * 1E-9 + 1E-9;
